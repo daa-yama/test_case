d3adebc01272bef6b602ce41f61d1780df231caa
daa-yama <rudie_1987@yahoo.co.jp>
2025-08-03 13:23:55 +0900

    test4

--- test4.py
+++ test4.py
@@ -1,7 +1,9 @@
-def check_age(age):
-    if age >= 18:
-        print("大人です")
-    else :
-        print("未成年です")
-
-check_age(19)
+def check_price_category(price):
+    if price >= 1000:
+        print("高価な商品です")
+    elif price >= 500:
+        print("普通の商品です")
+    else:
+        print("お買い得です")
+        
+check_price_category(200)
